7aa8ee7146d6765db3bca91e46419be1dba113a9
koekto <koekto2021@mail.ru>
2024-09-29 19:09:44 +0300

    modified:   app/dashboard/invoices/page.tsx 	modified:   app/dashboard/layout.tsx 	new file:   app/dashboard/loading.tsx 	modified:   app/dashboard/page.tsx 	modified:   app/lib/data.ts 	modified:   app/page.tsx 	modified:   app/ui/dashboard/cards.tsx 	modified:   app/ui/dashboard/latest-invoices.tsx 	modified:   app/ui/dashboard/revenue-chart.tsx 	modified:   app/ui/search.tsx 	modified:   next.config.mjs

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ import Image from 'next/image';
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
-      <div className={styles.shape}></div>
+      {/* <div className={styles.shape}></div> */}
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
         <AcmeLogo/>
       </div>
@@ -17,7 +17,7 @@ export default function Page() {
         
           <p className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
             <strong>Welcome to Acme.</strong> This is the example for the{' '}
-            <a href="https://nextjs.org/learn/" className="text-blue-500">
+            <a href="/dashboard" className="text-blue-500">
               Next.js Learn Course
             </a>
             , brought to you by Vercel.
